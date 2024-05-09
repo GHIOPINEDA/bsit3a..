8319c37126a76aaa7a4bc67c6889b65c892c9161
GHIOPINEDA <marcghiopinedaa@gmail.com>
2024-05-09 11:49:41 +0800

    Added my project

---
+++ layouts/auth.vue
@@ -0,0 +1,7 @@
+<template>
+    <v-app>
+        <v-img src="https://images.rawpixel.com/image_800/czNmcy1wcml2YXRlL3Jhd3BpeGVsX2ltYWdlcy93ZWJzaXRlX2NvbnRlbnQvbHIvdHAyNDQtYmcxLTAxXzEuanBn.jpg" height="100vh">
+        <Nuxt />
+    </v-img>
+    </v-app>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,92 @@
+<template>
+
+  <div class=" d-flex justify-center align-center" style="height:100vh;">
+    <v-card class="mx-auto" width="500" style="background-color: rgba(250,0,0,0)">
+
+      <div align="center" class="pa-5">
+        <v-img src="logo.png" width="200">
+
+
+        </v-img>
+      </div>
+
+      <div class="mx-6">
+        <v-card-text>
+          <v-row>
+            <v-col cols="12" md="6">
+              <v-btn block outlined>
+
+                <v-icon color="red">
+                  mdi-google
+                </v-icon>
+                google
+              </v-btn>
+            </v-col>
+
+
+
+            <v-col cols="12" md="6">
+              <v-btn block outlined>
+
+                <v-icon color="blue">
+                  mdi-facebook
+                </v-icon>
+                Facebook
+              </v-btn>
+            </v-col>
+          </v-row>
+        </v-card-text>
+
+
+        <v-card-text>
+          <v-form ref="form" v-model="valid" lazy-validation>
+            <v-text-field outlined label="Email" prepend-inner-icon="mdi-email" :rules="emailRules" required>
+
+            </v-text-field>
+            <v-text-field outlined label="Password" prepend-inner-icon="mdi-lock " :rules="passRules" required>
+
+            </v-text-field>
+            <v-btn class="mx-auto" block color="blue" @click="sumbit" :disabled="!valid">
+              Sign In
+            </v-btn>
+
+
+
+
+          </v-form>
+        </v-card-text>
+      </div>
+    </v-card>
+  </div>
+
+</template>
+
+
+
+<script>
+export default {
+  layout: "auth",
+  data: () => ({
+    valid: true,
+    password: "",
+    passRules: [(v) => !!v || "Password is required"],
+    emailRules: [
+    v => !!v || 'E-mail is required',
+        v => /.+@.+\..+/.test(v) || 'E-mail must be valid',
+
+
+    ],
+  }),
+
+
+  methods: {
+    sumbit() {
+      if (this.$refs.form.validate()) {
+        console.log("Successfully Login");
+        this.$router.push("/dashboard");
+      }
+    },
+
+  },
+}
+</script>
